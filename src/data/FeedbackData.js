d8a01529f262c57f7407f5e062a424153a23b493
Sarah Marion <sarahomarion@gmail.com>
2023-02-08 12:01:23 +1100

    initial

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,38 @@
+const FeedbackData=[
+    {
+        id: 1,
+        rating: 10,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .'
+    },
+    {
+        id:2,
+        rating:7,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .'
+    },
+    {
+        id:3,
+        rating:9,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .'
+    },
+    {
+        id:4,
+        rating:5,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .'
+    },
+    {
+        id:5,
+        rating:8,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .'
+    },
+    {
+        id:6,
+        rating:10,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .'
+    },
+    {
+        id:7,
+        rating:1,
+        text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor .'
+    },
+]
+export default FeedbackData
